816c72b42914b048428651d8b462dc5ea9cfe53f
exploitativity <firerider521@gmail.com>
2014-07-24 14:46:09 -0700

    Final
    variable changes

--- shinku_tatsumaki/SorterBot9000.c
+++ shinku_tatsumaki/SorterBot9000.c
@@ -1,46 +1,52 @@
 //This program will, obviously, sort the balls by their color by utilizing the reflective sensor to measure the value.
 //If it detects a red ball, a servo will move to let the ball into one box. A blue ball will move to another box.
 
-int blueValue=40;
+int blueValue=30;
 int redValue=500;
 int variableValue=50;
-int gatePort=2;
-int raisedGate=400;
-int loweredGate=1204;
-int tilterPort=1;
+
+int gatePort=1;
+int raisedGate=300;
+int loweredGate=1300;
+
+int tilterPort=0;
 int leftTilt=400;
 int centeredTilt=548;
 int rightTilt=676;
+
 int main()
 {
+	enable_servos();
 	while(1)
 	{
-		if(analog(5)>blueValue-variableValue && analog(5)< blueValue+variableValue)
+		if(analog(0)>blueValue-variableValue && analog(0)< blueValue+variableValue)
 		{
-			set_servo_position(gatePort,raisedGate);
-			msleep(2000);
+			printf("DETECTED BLUE BALL, MOVING LEFT\n");
 			set_servo_position(tilterPort,leftTilt);
-			msleep(3000);
-			set_servo_position(tilterPort,centeredTilt);
-			msleep(3000);
-			set_servo_position(gatePort,loweredGate);
-			msleep(2000);
-		}
-		else if(analog(5)>redValue-variableValue && analog(5)<redValue+variableValue)
-		{
+			msleep(1500);
 			set_servo_position(gatePort,raisedGate);
-			msleep(2000);
-			set_servo_position(tilterPort,rightTilt);
-			msleep(3000);
-			set_servo_position(tilterPort,centeredTilt);
-			msleep(3000);
+			msleep(1000);
 			set_servo_position(gatePort,loweredGate);
-			msleep(2000);
+			msleep(1000);
+			set_servo_position(tilterPort,centeredTilt);
+			msleep(1500);
+		}
+		else if(analog(0)>redValue-variableValue && analog(0)<redValue+variableValue)
+		{
+			printf("DETECTED RED BALL, MOVING RIGHT\n");
+			set_servo_position(tilterPort,rightTilt);
+			msleep(1500);
+			set_servo_position(gatePort,raisedGate);
+			msleep(1000);
+			set_servo_position(gatePort,loweredGate);
+			msleep(1000);
+			set_servo_position(tilterPort,centeredTilt);
+			msleep(1500);
 		}
 		else
 		{
-			msleep(2000);
-			printf("CHECKING BALL.\n");
+			msleep(1000);
+			printf("NO BALL DETECTED. CHECKING...\n");
 		}
 		
 	}
