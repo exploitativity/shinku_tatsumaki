838dc332eac668b1d891c7525ea45478894867b3
exploitativity <firerider521@gmail.com>
2014-07-24 13:19:43 -0700

    I am an adult.

--- shinku_tatsumaki/SorterBot9000.c
+++ shinku_tatsumaki/SorterBot9000.c
@@ -1,29 +1,30 @@
 //This program will, obviously, sort the balls by their color by utilizing the reflective sensor to measure the value.
 //If it detects a red ball, a servo will move to let the ball into one box. A blue ball will move to another box.
 
-int blueValue=2;
+int blueValue=200;
 int redValue=54;
+int variableValue=50
 int main()
 {
 	while(1)
 	{
-		if(analog(5)>blueValue-100 && analog(5)< blueValue+100)
+		if(analog(5)>blueValue-variableValue && analog(5)< blueValue+variableValue)
 		{
-			set_servo_position(rwhr923yurfewhriwhf,1urwhf8g3rg)
-			msleep(3000)
-			set_servo_position(ewafoejfewjgfiosdjs,2hirheihewi)
-			msleep(3000)
+			set_servo_position(rwhr923yurfewhriwhf,1urwhf8g3rg);
+			msleep(3000);
+			set_servo_position(ewafoejfewjgfiosdjs,2hirheihewi);
+			msleep(3000);
 		}
-		else if(analog(5)>redValue-100 && analog(5)<redValue+100)
+		else if(analog(5)>redValue-variableValue && analog(5)<redValue+variableValue)
 		{
-			set_servo_position(rwhr923yurfewhriwhf,1urwhf8g3rg)
-			msleep(3000)
-			set_servo_position(ewafoejfewjgfiosdjs,2hirheihewi)
-			msleep(3000)
+			set_servo_position(rwhr923yurfewhriwhf,1urwhf8g3rg);
+			msleep(3000);
+			set_servo_position(ewafoejfewjgfiosdjs,2hirheihewi);
+			msleep(3000);
 		}
 		else
 		{
-			printf("PLEASE PLACE A BALL HERE.\n");
+			printf("PLEASE PLACE A BALL IN THE RECEIVER.\n");
 		}
 		
 	}
